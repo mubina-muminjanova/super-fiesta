6a3adc3a609823b32d21c44428f88bd632233326
Mubina Muminjanova <mubina.muminjanova1@gmail.com>
2026-03-19 16:49:45 +0500

    js

--- app.js
+++ app.js
@@ -1,61 +1,36 @@
-// Array Methods
+// Loops - takrorlash operatorlari
 
-const fruits = ["Apple", "Orange", "Apple", "Mango, 'Orange"];
+// while - davomida
+let num = 0; // 10,
 
-// 1. indexOF - array elemntlarining index ini topadi (arrayning boshidan qidiradi)
-console.log(fruits.indexOf('Orange'));
+// while (num < 100) {
+//   console.log(num);
+//   // infinity loop
+//   num = num + 1;
+// }
 
-// 2. lastIndexOf - array elementlarining index ini topadi (arrayning oxiridan qidiradi)
-console.log(fruits.lastIndexOf('Orange'));
+// do while
 
-// 3. includes - o'z ichiga olish // boolean
-console.log(fruits.includes('Mango'));
+// do {
+//     console.log(num);
+//     num = num + 1
+// } while (num < 10)
 
-// 4. find - shartni bajaruvchi birinchi elementni qaytaradi 
-// const myFruit = fruits.find(el => el.startsWith('A'))
-// console.log(myFruit);
+// for - indexlar muhim bo'lganda ishlatiladi
+const sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// 5. findIndex - shartni bajaruvchi birinchi elementning indexini qaytaradi 
-const myFruit = fruits.findIndex(el => el.startsWith('A'))
-console.log(myFruit);
+// for(let i = 0; i < arr.length; i++){
+//     console.log(i);
+// }
 
-// 6. findLast - shartni bajaruvchi oxirgi elementni topadi
-// let numbers =[10, 20, 30, 40, 30]
-// let result = numbers.findLast(num => num => 25)
-// console.log(result); // 30
-
-// 7. findlastIndex - oxirgi mos keladigan elementning indexini qaytaradi
-let numbers = [10, 20, 30, 40, 30]
-let index = numbers.findLastIndex(num => num => 25)
-console.log(index); // 4
-
-// 8. isArray - uzatilgan paramentrning array yoki yo'qligini tekshirish
-const nums = [1, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(Array.isArray(fruits)); //true
-console.log(Array.isArray(nums)); //false
-
-// 9. forEach - arrayning har bir elementi ustida amal bajarish
-fruits.forEach(el => {
-    console.log(el.toUpperCase());
-})
-nums.forEach(el => {
-    console.log(Math.pow(el, 2));
-})
-
-// 10. filter // boolean
-nums.filter(el => {
-    console.log(el > 5);
-})
-
-let newNum = 5;
-
-if (newNum > 5) {
-    console.log("Katta");
-} else {
-    console.log("Juft");
+// for of - index muhim bo'lmaganda ishlatiladi
+for (son of sonlar) {
+  console.log(son ** 2);
 }
 
-const toq_sonlar = []
-const juft_sonlar = []
+// foreach
+sonlar.forEach((el) => {
+  console.log(el * 2);
+});
 
-// forEach -> nums -> if el % 2 == 1 ? toq_sonlar : juft_sonlar
+// for in - Object
