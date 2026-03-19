a25dcdbef4fe617ba135c2e76255aed003cacb58
Mubina Muminjanova <mubina.muminjanova1@gmail.com>
2026-03-19 16:49:19 +0500

    js

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
-// Array methods
-// 1. push - arrayga yangi element qo'shadi (oxiriga)
-const numbers = [1, 2, 3, 4, 5];
-numbers.push()
-console.log(1, 2, 3, 4);
+// Array Methods
 
-// 2. pop - arrayning oxirgi elementini o'chiradi
-numbers.pop()
-console.log(numbers);
+const fruits = ["Apple", "Orange", "Apple", "Mango, 'Orange"];
 
-// 3. shift - arrayning birinchi elemntini o'chiradi
-numbers.shift()
-console.log(numbers);
+// 1. indexOF - array elemntlarining index ini topadi (arrayning boshidan qidiradi)
+console.log(fruits.indexOf('Orange'));
 
-// 4. unshift - arrayga yangi element qo'shadi (boshiga)
-numbers.unshift(0)
-console.log(numbers);
+// 2. lastIndexOf - array elementlarining index ini topadi (arrayning oxiridan qidiradi)
+console.log(fruits.lastIndexOf('Orange'));
 
-// 5. concat - ikkita arrayni qo'shib yangi array yaratish uchun ishlatiladi
-const newNums = [10, 11, 12, 13]
-const myNums = numbers.concat(newNums)
-console.log(myNums);
+// 3. includes - o'z ichiga olish // boolean
+console.log(fruits.includes('Mango'));
 
-// 6. slice - qirqish, nusxalash
-const names = ['Mubina', 'Farida', 'Zarina', 'Shaxzoda']
-const newArr = names.slice(0,3)
-console.log(newArr);
+// 4. find - shartni bajaruvchi birinchi elementni qaytaradi 
+// const myFruit = fruits.find(el => el.startsWith('A'))
+// console.log(myFruit);
 
-// 7. splice - index bo'yicha array elemntlarini o'zgartiradi
-names.splice(0, 3, 'New name 1', 'New name 2')
-console.log(names);
+// 5. findIndex - shartni bajaruvchi birinchi elementning indexini qaytaradi 
+const myFruit = fruits.findIndex(el => el.startsWith('A'))
+console.log(myFruit);
 
-// 8. join - qo'shish
-const str = ['A', 'B', 'C']
-console.log(str.join(''));
+// 6. findLast - shartni bajaruvchi oxirgi elementni topadi
+// let numbers =[10, 20, 30, 40, 30]
+// let result = numbers.findLast(num => num => 25)
+// console.log(result); // 30
 
-// 9. reverse - teskari aylantirish
-console.log(str,reverse());
+// 7. findlastIndex - oxirgi mos keladigan elementning indexini qaytaradi
+let numbers = [10, 20, 30, 40, 30]
+let index = numbers.findLastIndex(num => num => 25)
+console.log(index); // 4
 
-// 10. split - stringni arrayga o'giradi
-const text = 'lorem ipsum dolor'
-console.log(text.split(''));
+// 8. isArray - uzatilgan paramentrning array yoki yo'qligini tekshirish
+const nums = [1, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(Array.isArray(fruits)); //true
+console.log(Array.isArray(nums)); //false
 
+// 9. forEach - arrayning har bir elementi ustida amal bajarish
+fruits.forEach(el => {
+    console.log(el.toUpperCase());
+})
+nums.forEach(el => {
+    console.log(Math.pow(el, 2));
+})
+
+// 10. filter // boolean
+nums.filter(el => {
+    console.log(el > 5);
+})
+
+let newNum = 5;
+
+if (newNum > 5) {
+    console.log("Katta");
+} else {
+    console.log("Juft");
+}
+
+const toq_sonlar = []
+const juft_sonlar = []
+
+// forEach -> nums -> if el % 2 == 1 ? toq_sonlar : juft_sonlar
