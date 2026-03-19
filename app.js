29fc2ab4007693058329aed5c188ce066de49782
Mubina Muminjanova <mubina.muminjanova1@gmail.com>
2026-03-19 16:48:47 +0500

    javascript

--- app.js
+++ app.js
@@ -1,50 +1,43 @@
-// // 
-// // 
-// // 
-// // 
-// // 
-// // 
-// // symbol
+// Array methods
+// 1. push - arrayga yangi element qo'shadi (oxiriga)
+const numbers = [1, 2, 3, 4, 5];
+numbers.push()
+console.log(1, 2, 3, 4);
 
-// // NonPrimitive Data Types
-// // Object -> Array, Function
+// 2. pop - arrayning oxirgi elementini o'chiradi
+numbers.pop()
+console.log(numbers);
 
-// const cars = ['Cobalt', 'BMW', 'Damas', 'Gentra', 10, true]
-// // Array methods
-// console.log(cars.length); // uzunlik
-// console.log(cars[6]); // index
+// 3. shift - arrayning birinchi elemntini o'chiradi
+numbers.shift()
+console.log(numbers);
 
-//  // array elementlarini index yordamida o'zgartiruvchi
-// cars[0] = 'Nexia 3'
-// console.log(cars[0]);
+// 4. unshift - arrayga yangi element qo'shadi (boshiga)
+numbers.unshift(0)
+console.log(numbers);
 
-// // array elementlarini stringga o'girish
-// console.log(cars,toString());
+// 5. concat - ikkita arrayni qo'shib yangi array yaratish uchun ishlatiladi
+const newNums = [10, 11, 12, 13]
+const myNums = numbers.concat(newNums)
+console.log(myNums);
 
-// // array elementlarining qaysi ma'lumot turida ekanini tekshirish
-// console.log(typeof(cars[8]));
+// 6. slice - qirqish, nusxalash
+const names = ['Mubina', 'Farida', 'Zarina', 'Shaxzoda']
+const newArr = names.slice(0,3)
+console.log(newArr);
 
-// // array elementlarini tartiblash
-// console.log(cars.sort());
+// 7. splice - index bo'yicha array elemntlarini o'zgartiradi
+names.splice(0, 3, 'New name 1', 'New name 2')
+console.log(names);
 
-// // arrayning oxirgi index iga ulanish
-// console.log(cars[cars.length - 1]);
+// 8. join - qo'shish
+const str = ['A', 'B', 'C']
+console.log(str.join(''));
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-const numbers = [1, 2, 3, 4, 32]
+// 9. reverse - teskari aylantirish
+console.log(str,reverse());
 
-// for lopp - takrorlash operatori
-
-for(a of fruits) {
-    console.log(a);
-}
-
-for (key of numbers) {
-    if(key % 2 == 1) {
-     console.log(key, "toq son");
-    } else {
-        console.log(key, "juft son");
-    }
-}
-
+// 10. split - stringni arrayga o'giradi
+const text = 'lorem ipsum dolor'
+console.log(text.split(''));
 
